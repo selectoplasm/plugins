de9643cdb21df696a49cc88427e39afb504d3e89
Jim Bridger <retrotheft@gmail.com>
2025-03-29 11:28:16 +1100

    fixed borders

--- borders/worker.js
+++ borders/worker.js
@@ -3,23 +3,24 @@ importScripts("/plugins/sharedScripts.js")
 self.onmessage = (event) => {
    const { data } = event;
    const result = run(data);
+   console.log(result)
    self.postMessage(result);
 }
 
-const borderFn = (i) => createRuleset(`.b${i}`, ["border-width", `${i}px`])
-const borderRadiusFn = (i, initialValue) => createRuleset(`.br${i}`, ["border-radius", `${i * initialValue}px`])
+const borderFn = (i) => createRuleset(`.b${i}`, [["border-width", `${i}px`]])
+const borderRadiusFn = (i, initialValue) => createRuleset(`.br${i}`, [["border-radius", `${i * initialValue}px`]])
 
 function run(state) {
    const NUM_ITERATIONS = 10
    const rulesets = [
-      createRuleset(".solid", ["border-style", "solid" ]),
-      createRuleset(".dashed", ["border-style", "dashed"]),
-      createRuleset(".dotted", ["border-style", "dotted"]),
-      createRuleset(".double", ["border-style", "double"]),
-      createRuleset(".groove", ["border-style", "groove"]),
-      createRuleset(".ridge", ["border-style", "ridge"]),
-      createRuleset(".inset", ["border-style", "inset"]),
-      createRuleset(".outset", ["border-style", "outset"]),
+      createRuleset(".solid", [["border-style", "solid" ]]),
+      createRuleset(".dashed", [["border-style", "dashed"]]),
+      createRuleset(".dotted", [["border-style", "dotted"]]),
+      createRuleset(".double", [["border-style", "double"]]),
+      createRuleset(".groove", [["border-style", "groove"]]),
+      createRuleset(".ridge", [["border-style", "ridge"]]),
+      createRuleset(".inset", [["border-style", "inset"]]),
+      createRuleset(".outset", [["border-style", "outset"]]),
    ]
    for (let i = 0; i < NUM_ITERATIONS; i++) {
       rulesets.push(borderFn(i))
