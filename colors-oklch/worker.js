c88232fe19fcebf97c2f6eeee76770e0b0519535
Jim Bridger <retrotheft@gmail.com>
2025-04-07 11:17:40 +1000

    renamed color to color-string, added comment in preview.css

--- colors-oklch/worker.js
+++ colors-oklch/worker.js
@@ -27,7 +27,7 @@ function run(config) {
 
 function generateCss(lThreshold) {
    let string = `:root {\n\t--l-threshold: ${lThreshold};\n}`
-   string += `*[style*="--color"] {\n\t--l: clamp(0, (var(--l-threshold) / l - 1) * infinity, 1);\n\t--readable-color: oklch(from var(--color) var(--l) 0 h);\n}`
+   // string += `*[style*="--color"] {\n\t--l: clamp(0, (var(--l-threshold) / l - 1) * infinity, 1);\n\t--readable-color: oklch(from var(--color) var(--l) 0 h);\n}`
    return string.trim()
 }
 
@@ -39,7 +39,7 @@ function generateHtmlAndTokens(name, hue, { center, width, steepness, midpoint, 
       const chroma = hue === -1 ? 0 : chromaCurve(i) // neutral sends hue as -1
       const color = `oklch(${lightness}% ${chroma} ${hue})`
       tokens.push([`--color-${name.toLowerCase()}-${i * 100}`, color])
-      html += `<div style='flex-grow: 1; --color: var(--color-${name}-${i * 100}); background-color: var(--color); color: var(--readable-color);'>${i * 100}</div>`
+      html += `<div style='flex-grow: 1; --color-string: var(--color-${name}-${i * 100}); background-color: var(--color-string); color: var(--readable-color);'>${i * 100}</div>`
    }
    html += `</section>`
    return { html, tokens }
